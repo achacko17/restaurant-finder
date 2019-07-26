0454420d4da9a4167ad2355353f4a1c35161e098
Adheena Chacko <adheena_m_chacko@homedepot.com>
2019-07-26 16:25:51 -0400

    adds search bar to search restaurant categories

--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 //no component
 import {Container, Row, Col, Button, Input} from 'reactstrap';
+import { Spinner, Card, CardText, CardBody} from 'reactstrap';
+
 //with component
 import Dropdown from '../../components/Dropdown/Dropdown'
 import NavBar from '../../components/Navbar/NavBar'
-import Card from '../../components/Card/Card'
+// import Card from '../../components/Card/Card'
 import './Main.css'
 import axios from 'axios'
  
@@ -12,25 +14,34 @@ class Main extends Component{
 
     state={
         data: [],
-        text: ' ',
-   
+        result: []
     }
 
     handleChange = (event)  => {
-      this.setState({text: event.target.value})
-      console.log(this.state.text)
+        console.log(this.state.data)
+    //   this.setState({text: event.target.value})
+    //   console.log(this.state.text)
+
+    const text = event.target.value.toLowerCase()
+      const newResult = []
+      for(let index = 0; index < this.state.data.length; index++){
+          if(this.state.data[index].categories.name.toLowerCase().includes(text)){
+              newResult.push(this.state.data[index])
+          }
+      }
+
+      this.setState({result:newResult})
     }
 
-    componentDidMount(){
 
+
+    componentDidMount(){
         let config = {
             headers: {"user-key": "0bebf79759ac7e47316ab07b4578eb33"} 
         }
-
         axios.get( 'https://developers.zomato.com/api/v2.1/categories', config, {} )
-
             .then( response => {
-                this.setState({ data: response.data})
+                this.setState({ data: response.data.categories, result: response.data.categories})
                 console.log(response.data)
             } )
             .catch(error => {
@@ -38,7 +49,25 @@ class Main extends Component{
             });
     }
 
+    renderCategories(){
+        return this.state.result.map(category => {
+            return (
+                <Card key={category.categories.name}>
+                    <CardBody>
+                        <CardText>{category.categories.name}</CardText>
+                    </CardBody>
+                </Card>
+                // console.log(category.categories.name)
+            )
+        })
+    }
+
+
+
     render() {
+
+        let resp = this.state.isLoading ? <Spinner color="secondary" /> : this.renderCategories()
+
         return(
             <>
             <div>
@@ -49,12 +78,14 @@ class Main extends Component{
             <Container>
                 <Row>
                     <Col md="8">
-                        {/* <Card/> */}
-                        <Input onChange={this.handleChange}/>
+                        {resp}
+                        
                         {this.state.text}
                     </Col>
                     <Col md="4">
-                        <Dropdown/>
+                       
+                        <Input placeholder="search categories" onChange={this.handleChange}/>
+                         <Dropdown/>
                         <Button color='primary' className='fourthButton'>Search</Button>
                     </Col>
                 </Row>
